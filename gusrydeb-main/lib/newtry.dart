4fd3eea0931da59b66e84ace3043663185a0bce3
Ebba Rydnell <ebba.ryd@gmail.com>
2022-09-09 16:27:18 +0200

    Steg 1 (icon button does not work)

---
+++ gusrydeb-main/lib/newtry.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainView(),
+    );
+  }
+}
+
+class MainView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+               Text("here is some text"),
+               TextField(),
+              ],
+            ),
+        ),
+     );
+  }
+
+
+}

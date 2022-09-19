e0e71541399e522d068b72d44aee6c85db701800
Ebba Rydnell <ebba.ryd@gmail.com>
2022-09-19 19:40:35 +0200

    Half done steg 2

--- gusrydeb-main/lib/main.dart
+++ gusrydeb-main/lib/main.dart
@@ -1,99 +1,180 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
+import 'dart:js';
 
-void main() => runApp(const MyApp());
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  var state = MyState();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => state,
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'TIG169 TODO';
     return MaterialApp(
-      title: appTitle,
+      title: "TIG169 TODO",
       theme: ThemeData(
         primarySwatch: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(appTitle),
-          actions: [
-            IconButton(
-              icon: Icon(
-                Icons.keyboard_arrow_left_outlined,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => secondView()));
-              },
-            )
-          ],
-        ),
-        body: const MyCustomForm(),
-      ),
+      home: TodoListView(),
     );
   }
 }
 
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 28),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'What are you going to do?',
-              ),
-            )),
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: Container(
-              child: Center(
-                child: Text(
-                  "+ ADD",
-                  style: TextStyle(color: Colors.black, fontSize: 22),
-                ),
-              ),
-            )),
-      ],
-    );
-  }
-}
-
-class secondView extends StatelessWidget {
+class TodoListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: _list(),
+      appBar: AppBar(
+        title: Text("TIG169 TODO"),
+        centerTitle: true,
+      ),
+      body: Consumer<MyState>(
+        builder: (context, state, child) => Todolist(state.list),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          var newCard = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AddToDoView(ToDo(
+                        message: "",
+                      ))));
+          if (newCard != null) {
+            Provider.of<MyState>(context, listen: false).addCard(newCard);
+          }
+        },
+      ),
     );
   }
+}
 
-  Widget _list() {
+class Todolist extends StatelessWidget {
+  final List<ToDo> list;
+
+  Todolist(this.list);
+
+  Widget build(BuildContext context) {
     return ListView(
-      children: [
-        _item("Write a book"),
-        _item("Do homework"),
-        _item("Tidy room"),
-        _item("Watch TV"),
-        _item("Nap"),
-        _item("Shop groceries"),
-        _item("Have fun"),
-        _item("Meditate"),
-      ],
-    );
+        children: list.map((card) => _cardItem(context, card)).toList());
   }
 
-  Widget _item(text) {
+  Widget _cardItem(context, card) {
+    return ListTile(
+        title: Text(card.message),
+        trailing: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            var state = Provider.of<MyState>(context, listen: false);
+            state.removeCard(card);
+          },
+        ));
+  }
+}
+
+class ToDo {
+  String message;
+
+  ToDo({required this.message});
+}
+
+class MyState extends ChangeNotifier {
+  List<ToDo> _list = [];
+
+  List<ToDo> get list => _list;
+
+  void addCard(ToDo card) {
+    _list.add(card);
+    notifyListeners();
+  }
+
+  void removeCard(ToDo card) {
+    _list.remove(card);
+    notifyListeners();
+  }
+}
+
+class AddToDoView extends StatefulWidget {
+  final ToDo card;
+
+  AddToDoView(this.card);
+
+  @override
+  State<StatefulWidget> createState() {
+    return AddToDoViewState(card);
+  }
+}
+
+class ToDoWidget extends StatelessWidget {
+  final ToDo card;
+
+  ToDoWidget(this.card);
+
+  Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Text(text, style: TextStyle(fontSize: 36)),
-    );
+        padding: const EdgeInsets.all(20.0), child: Text(card.message));
+  }
+}
+
+class AddToDoViewState extends State<AddToDoView> {
+  String message = " ";
+
+  TextEditingController textEditingController = TextEditingController();
+  
+
+  AddToDoViewState(ToDo card) {
+    this.message = card.message;
+
+    textEditingController = TextEditingController(text: card.message);
+
+    textEditingController.addListener(() {
+      setState(() {
+        message = textEditingController.text;
+      });
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("TIG169 TODO"), 
+        centerTitle: true, 
+        ),
+        body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(height: 24),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: 
+              TextField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(width: 5.0, color: Colors.black)), 
+                  hintText: 'What are you going to do?'),
+                ), 
+                ),
+            Container(height: 24),
+            TextButton(
+            child: Text("ADD +", style: TextStyle(color: Colors.black, fontWeight: FontWeight. bold)),
+            onPressed: () {
+              Navigator.pop(context, ToDo(message: message));
+            },
+          )
+          ]
+        )
+        )
+            );
   }
 }

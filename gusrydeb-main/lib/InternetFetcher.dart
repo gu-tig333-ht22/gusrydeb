6fc072d80bed606c6f4fbc8b661c200e3009a7ba
Ebba Rydnell <ebba.ryd@gmail.com>
2022-09-23 16:31:34 +0200

    new file. Steg 3 not finished

--- gusrydeb-main/lib/InternetFetcher.dart
+++ gusrydeb-main/lib/InternetFetcher.dart
@@ -14,9 +14,11 @@ import 'dart:convert';
 
 class InternetFetcher {
   static Future<String> fetchIp() async {
-    http.Response response = await http.get(Uri.parse('https://todoapp-api.apps.k8s.gu.se/'));
+    http.Response response =
+        await http.get(Uri.parse('https://todoapp-api.apps.k8s.gu.se/'));
     var jsonData = response.body;
     var obj = jsonDecode(jsonData);
+    print(obj['ip']);
     return obj['ip'];
   }
 }
